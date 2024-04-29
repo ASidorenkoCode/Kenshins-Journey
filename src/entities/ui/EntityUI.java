15912689df825f332c089152c6e7e0627f389530
comhendrik <76622601+comhendrik@users.noreply.github.com>
2024-04-29 10:24:22 +0200

    started with position and animations for Player and PlayerUI

--- src/entities/ui/EntityUI.java
+++ src/entities/ui/EntityUI.java
@@ -1,12 +1,17 @@
 package entities.ui;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 abstract public class EntityUI {
     protected BufferedImage[][] animations;
+    protected int aniTick;
+
+    protected int aniIndex;
+    protected final static int ANI_SPEED = 25;
     abstract void drawAttackBox();
     abstract void drawHitBox();
     abstract void loadAnimations();
-    abstract void drawAnimations();
+    abstract void drawAnimations(Graphics g);
     abstract void drawHealthBar();
 }
